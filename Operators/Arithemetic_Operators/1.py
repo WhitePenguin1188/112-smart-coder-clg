c9ac5550ed20fc92c7d3e9ed028bcff85f911b8a
Whitepenguin <169655889+WhitePenguin1188@users.noreply.github.com>
2026-09-07 09:45:36 +0000

    Hi I am doing commit

--- Operators/Arithemetic_Operators/1.py
+++ Operators/Arithemetic_Operators/1.py
@@ -1,8 +1,10 @@
+#Read two numbers and print their sum, difference, product, quotient, and remainder
+
 a=int(input("Enter a value: "))
 b=int(input("Enter a value: "))
 
 print("Sum of 2 values :",a+b)
-print("Difference bertween 2 values:",a-b)
+print("Difference between 2 values:",a-b)
 print("Product of 2 values:",a*b)
 print("Quotient:",a/b)
 print("Remainder:",a%b)
