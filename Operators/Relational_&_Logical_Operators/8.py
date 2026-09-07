73eabf5893b11cd31eced16908000f0ffe198f7e
Whitepenguin <169655889+WhitePenguin1188@users.noreply.github.com>
2026-09-07 09:35:57 +0000

    Hi I am doing commit

--- Operators/Relational_&_Logical_Operators/8.py
+++ Operators/Relational_&_Logical_Operators/8.py
@@ -2,11 +2,7 @@ a=int(input("Enter the value for a: "))
 b=int(input("Enter the value for b: "))
 c=int(input("Enter the value for c: "))
 
-if a>b and a>c:
-    print("a is the greatest")
-elif b>a and b>c:
-    print("b is the greatest")
-elif c>a and c>b:
-    print("c is the greatest")
+if a&b==c:
+    print("All 3 are equal")
 else:
-    print("All are equal")
+    print("All 3 are not equal")
